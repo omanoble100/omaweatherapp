ca0705942c3b13d1d1e9829233556ae0d9b24133
omanoble100 <amosoma100@gmail.com>
2022-08-06 01:14:22 +0100

    Degree solved

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const myBtn = document.getElementById('click');
             
             const userInput = document.getElementById('userInput').value;
             const myKey = 'aa8767d5b6f2ebd1684c6b1e5d4f24da';
-            const endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=${myKey}` ;
+            const endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&units=imperial&appid=${myKey}` ;
             const imageUrl = ' http://openweathermap.org/img/wn/';
             const imgOutput = document.getElementById('display-weather')
             const weatherName = document.getElementById('weatherName')
