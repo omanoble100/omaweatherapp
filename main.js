9fd59e1479949b41911eb19e1533cc877980b315
omanoble100 <amosoma100@gmail.com>
2022-08-14 00:03:18 +0100

    color and output change

--- main.js
+++ main.js
@@ -8,17 +8,25 @@ const myBtn = document.getElementById('click');
             const endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&units=imperial&appid=${myKey}` ;
             const imageUrl = ' http://openweathermap.org/img/wn/';
             const imgOutput = document.getElementById('display-weather')
-            const weatherName = document.getElementById('weatherName')
+            // const weatherName = document.getElementById('weatherName')
+            const country = document.querySelector('.country-output')
+            
 
             fetch(endpoint)
             .then(response => response.json())
-            .then(json => {        
-                
+            .then(json => {   
                 const imgView = imageUrl + json.weather[0].icon +'.png';
                 const value = json.main.temp;
                 const degree = Math.floor(value)
-                imgOutput.innerHTML = `<img src='${imgView}'> <h1>${degree}°</h1> <img src='${imgView}'> `;
-                weatherName.innerText = `${userInput}: ${json.weather[0].main}`;
+                imgOutput.style.display ='flex';
+                imgOutput.innerHTML = `<img src='${imgView}'> <h1>${degree}°C</h1> `;
+                country.innerText = `${userInput}: ${json.weather[0].main}`;
+                country.style.color = 'rgb(68, 7, 77)'
+                weatherName.innerHTML = `<p>Lon: ${Math.floor(json.coord.lon)}º</p> - <p> Lat: ${Math.floor(json.coord.lat)}º</p>`;
+                weatherName.style.color ='rgb(68, 7, 77)';
+                
+            
+
             })
         }
         
